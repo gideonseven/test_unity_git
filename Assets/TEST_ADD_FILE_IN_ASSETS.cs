94007ab08b22df469c2fd63bf58a8e29751a1521
Rohit Dangol <rohit.dangol@griffithuni.edu.au>
2025-05-07 14:18:09 +1000

    test

--- Assets/TEST_ADD_FILE_IN_ASSETS.cs
+++ Assets/TEST_ADD_FILE_IN_ASSETS.cs
@@ -7,7 +7,8 @@ public class TEST_ADD_FILE_IN_ASSETS : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //test (Rohit)
+
     }
 
     // Update is called once per frame
